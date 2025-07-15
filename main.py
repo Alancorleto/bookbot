a5f90cebef0ff55acaaa73c161f2507a3760f38b
alancorleto <corletoalan@gmail.com>
2025-07-15 15:47:54 -0300

    make main.py be able to receive book paths as arguments

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words, count_characters
+import sys
 
 
 def get_book_text(file_path) -> str:
@@ -8,20 +9,24 @@ def get_book_text(file_path) -> str:
 
 
 def main() -> None:
-    book_path: str = "books/frankenstein.txt"
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book_path}...")
-    
-    print("----------- Word Count ----------")
-    num_words: int = count_words(get_book_text("books/frankenstein.txt"))
-    print(f"Found {num_words} total words.")
-    
-    print("--------- Character Count -------")
-    for character_count in count_characters(get_book_text(book_path)):
-        if character_count["character"].isalpha():
-            print(f"{character_count['character']}: {character_count['count']}")
-    
-    print("============= END ===============")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path: str = sys.argv[1]
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {book_path}...")
+        
+        print("----------- Word Count ----------")
+        num_words: int = count_words(get_book_text("books/frankenstein.txt"))
+        print(f"Found {num_words} total words.")
+        
+        print("--------- Character Count -------")
+        for character_count in count_characters(get_book_text(book_path)):
+            if character_count["character"].isalpha():
+                print(f"{character_count['character']}: {character_count['count']}")
+        
+        print("============= END ===============")
 
 
 main()
