bcbed8120a078a870d334e30b87e1d17f2d52e8e
alancorleto <corletoalan@gmail.com>
2025-07-15 15:41:36 -0300

    sort characters by count and build a better formed report

--- main.py
+++ main.py
@@ -8,10 +8,20 @@ def get_book_text(file_path) -> str:
 
 
 def main() -> None:
+    book_path: str = "books/frankenstein.txt"
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    
+    print("----------- Word Count ----------")
     num_words: int = count_words(get_book_text("books/frankenstein.txt"))
-    print(f"{num_words} words found in the document")
-    for character, count in count_characters(get_book_text("books/frankenstein.txt")).items():
-        print(f"'{character}': {count}")
+    print(f"Found {num_words} total words.")
+    
+    print("--------- Character Count -------")
+    for character_count in count_characters(get_book_text(book_path)):
+        if character_count["character"].isalpha():
+            print(f"{character_count['character']}: {character_count['count']}")
+    
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -3,7 +3,7 @@ def count_words(text: str) -> int:
     return len(word_count)
 
 
-def count_characters(text: str) -> dict[str, int]:
+def count_characters(text: str) -> list[dict]:
     character_counts: dict[str, int] = {}
     for word in text.lower().split():
         for character in word:
@@ -11,4 +11,17 @@ def count_characters(text: str) -> dict[str, int]:
                 character_counts[character] = 1
             else:
                 character_counts[character] += + 1
-    return character_counts
+    sorted_character_counts: list[dict] = sort_characters(character_counts)
+    return sorted_character_counts
+
+
+def sort_on(character: dict) -> int:
+    return character["count"]
+
+
+def sort_characters(character_counts: dict[str, int]) -> list[dict]:
+    characters = []
+    for character, count in character_counts.items():
+        characters.append({"character": character, "count": count})
+    characters.sort(key=sort_on, reverse=True)
+    return characters
