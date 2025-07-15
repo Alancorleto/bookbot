590665b4d841c253c7380633f868ce13a305d4f5
alancorleto <corletoalan@gmail.com>
2025-07-15 14:52:04 -0300

    read file contents and print it

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(file_path) -> str:
+    with open(file_path) as file:
+        book_contents = file.read()
+        return book_contents
+
+
+def main() -> None:
+    print(get_book_text("books/frankenstein.txt"))
+
+
+main()
