71db9bebbc69814e408ea6612140253e864249ea
alancorleto <corletoalan@gmail.com>
2025-07-15 14:55:20 -0300

    count words

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ def get_book_text(file_path) -> str:
         return book_contents
 
 
+def count_words(book_path: str) -> int:
+    book_text = get_book_text(book_path)
+    words = book_text.split()
+    return len(words)
+
+
 def main() -> None:
-    print(get_book_text("books/frankenstein.txt"))
+    num_words: int = count_words("books/frankenstein.txt")
+    print(f"{num_words} words found in the document")
 
 
 main()
